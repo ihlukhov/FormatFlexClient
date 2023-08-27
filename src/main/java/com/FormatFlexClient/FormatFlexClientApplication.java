10f55bbcb0bfda17648f08cec89b79a0e2448b46
ihlukhov <duan2dotmon@gmail.com>
2023-08-27 22:45:45 +0200

    project initialization

---
+++ src/main/java/com/FormatFlexClient/FormatFlexClientApplication.java
@@ -0,0 +1,13 @@
+package com.FormatFlexClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormatFlexClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormatFlexClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/FormatFlexClient/FormatFlexClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FormatFlexClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormatFlexClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
